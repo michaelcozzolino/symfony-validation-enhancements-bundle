136ca3152577396d1364730b7e24684b55a93b5a
M.Cozzolino <michaelcozzolino@outlook.it>
2024-11-17 15:54:23 +0100

    feat(validation): implement EntityExistsValidator

---
+++ src/Validator/Constraint/EntityExists.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types=1);
+
+namespace MichaelCozzolino\SymfonyValidationEnhancementsBundle\Validator\Constraint;
+
+use ReflectionException;
+use Symfony\Component\Validator\Constraint;
+
+class EntityExists extends Constraint
+{
+    /**
+     * @param class-string          $entityClass
+     * @param non-empty-string      $entityProperty The property for which the entity must be retrieved
+     * @param non-empty-string|null $entityName     An identifier to show in the validation message, if null the entity
+     *                                              class short name will be used
+     * @param mixed|null            $options
+     * @param list<string>|null     $groups
+     * @param mixed|null            $payload
+     */
+    public function __construct(
+        protected readonly string  $entityClass,
+        protected readonly string  $entityProperty = 'id',
+        protected readonly ?string $entityName = null,
+        mixed                      $options = null,
+        array                      $groups = null,
+        mixed                      $payload = null
+    ) {
+        parent::__construct($options, $groups, $payload);
+    }
+
+    /**
+     * @return class-string
+     */
+    public function getEntityClass(): string
+    {
+        return $this->entityClass;
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    public function getEntityProperty(): string
+    {
+        return $this->entityProperty;
+    }
+
+    /**
+     * @throws ReflectionException
+     *
+     * @return string|null
+     */
+    public function getEntityName(): ?string
+    {
+        return $this->entityName ?? (new \ReflectionClass($this->entityClass))->getShortName();
+    }
+}
---
+++ src/Validator/Constraint/EntityExistsValidator.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace MichaelCozzolino\SymfonyValidationEnhancementsBundle\Validator\Constraint;
+
+use Doctrine\ORM\EntityManagerInterface;
+use ReflectionException;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+/**
+ * @psalm-suppress PropertyNotSetInConstructor $context is set in {@see ConstraintValidator::initialize}
+ */
+class EntityExistsValidator extends ConstraintValidator
+{
+    public function __construct(public readonly EntityManagerInterface $entityManager)
+    {
+    }
+
+    /**
+     * @param mixed      $value
+     * @param Constraint $constraint
+     *
+     * @throws ReflectionException
+     *
+     * @return void
+     */
+    public function validate(mixed $value, Constraint $constraint): void
+    {
+        if ($constraint instanceof EntityExists === false) {
+            throw new UnexpectedTypeException($constraint, EntityExists::class);
+        }
+
+        $repository = $this->entityManager->getRepository($constraint->getEntityClass());
+
+        $entity = $repository->findOneBy([
+            $constraint->getEntityProperty() => $value,
+        ]);
+
+        if ($entity === null) {
+            $this->context->addViolation("The requested `{$constraint->getEntityName()}` does not exist.");
+        }
+    }
+}
