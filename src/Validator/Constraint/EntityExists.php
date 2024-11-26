f4230d149c432d7da17f432401b83e058c52f756
M.Cozzolino <michaelcozzolino@outlook.it>
2024-11-26 22:44:07 +0100

    feat(validator): EntityExistsValidator - add possibility to validate non existence of an entity

--- src/Validator/Constraint/EntityExists.php
+++ src/Validator/Constraint/EntityExists.php
@@ -14,15 +14,18 @@ class EntityExists extends Constraint
 {
     /**
      * @param class-string          $entityClass
-     * @param non-empty-string      $entityProperty The property for which the entity must be retrieved
-     * @param non-empty-string|null $entityName     An identifier to show in the validation message, if null the entity
-     *                                              class short name will be used
+     * @param bool                  $validateExistence true if you want to validate the existence or false if you want
+     *                                                 to validate the non-existence of the entity
+     * @param non-empty-string      $entityProperty    The property for which the entity must be retrieved
+     * @param non-empty-string|null $entityName        An identifier to show in the validation message, if null the
+     *                                                 entity class short name will be used
      * @param mixed|null            $options
      * @param list<string>|null     $groups
      * @param mixed|null            $payload
      */
     public function __construct(
         protected readonly string  $entityClass,
+        protected readonly bool    $validateExistence = true,
         protected readonly string  $entityProperty = 'id',
         protected readonly ?string $entityName = null,
         mixed                      $options = null,
@@ -38,6 +41,11 @@ class EntityExists extends Constraint
     public function getEntityClass(): string
     {
         return $this->entityClass;
+    }
+
+    public function validateExistence(): bool
+    {
+        return $this->validateExistence;
     }
 
     /**
